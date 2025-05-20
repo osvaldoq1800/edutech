08b126cd1d34c5be5146943a5aeb9d75e7529181
Abelardo555 <129196916+Abelardo555@users.noreply.github.com>
2025-05-20 00:56:49 -0400

    modificacion 2

--- edutech/src/main/java/com/example/edutech/model/curso.java
+++ edutech/src/main/java/com/example/edutech/model/curso.java
@@ -1,11 +1,12 @@
 package com.example.edutech.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class curso {
     private String NombreCurso;
     private String DescripcionCurso;
--- edutech/src/main/java/com/example/edutech/model/usuario.java
+++ edutech/src/main/java/com/example/edutech/model/usuario.java
@@ -1,16 +1,58 @@
 package com.example.edutech.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
+public class usuario {
 
-public class usuario{
-    private String rut;
-    private String nombre;
-    private String apellido;
-    private String direccion;
-    private Integer edad; 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Idusuario;
+
+    @NotBlank(message = "Este RUT no puede estar vacio")
+    @Column(unique = true, length = 12, nullable = false)
+    private String Run;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(nullable = false)
+    private String Nombre;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(nullable = false)
+    private String Apellido;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(nullable = false)
+    private String Correo;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Size(min = 8, max = 32, message = "La contraseña debe tener entre 8 a 16 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\\\d).{8,}$", message = "La contraseña debe tener almenos una mayúscula, una minúscula y un número")
+    @Column(name = "Contraseña", nullable = false)
+    private String Contrasena;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(name = "Usuario", nullable = false)
+    private String Usuario;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(name = "Numero Celular", nullable = false)
+    private String NumeroCelular;
+
+    @NotBlank(message = "Este campo no puede estar vacio")
+    @Column(nullable = false)
+    private Data Edad;
 
 }
