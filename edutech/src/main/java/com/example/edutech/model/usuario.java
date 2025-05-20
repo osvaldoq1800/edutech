71fd97af5851d7079dd4fb02c40e7ec38fa5b337
osvaldoq1800 <osv.quiroga@duocuc.cl>
2025-05-19 22:24:13 -0400

    usuario

--- edutech/src/main/java/com/example/edutech/model/usuario.java
+++ edutech/src/main/java/com/example/edutech/model/usuario.java
@@ -6,6 +6,11 @@ package com.example.edutech.model;
 
 
 public class usuario{
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private Integer edad; 
     
 
 }
