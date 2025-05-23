ecb37734c294bb69e8143b33b9f292c9551ec104
sebastian zuñiga santibañez <138766871+yoelpixula69@users.noreply.github.com>
2025-05-23 02:35:34 -0400

    nuevo package repository y clase llamada CursoRepository

--- edutech/src/main/java/com/example/edutech/repository/CursoRepository.java
+++ edutech/src/main/java/com/example/edutech/repository/CursoRepository.java
@@ -1,5 +1,12 @@
 package com.example.edutech.repository;
 
-public interface CursoRepository {
 
+import com.example.edutech.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, String> {
+    // JpaRepository ya te da los métodos básicos CRUD
+    // Puedes agregar consultas personalizadas si las necesitas
 }
